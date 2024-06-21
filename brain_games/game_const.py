3761a3b5f263334e2ab492b3b1ecbc5294847f98
AntiViruS90 <slava90nikitin90@gmail.com>
2024-06-22 00:03:32 +0300

    new files in brain_games: game_const.py, gcd_enjine.py & brain_gcd.py

---
+++ brain_games/game_const.py
@@ -0,0 +1,7 @@
+GAME_INSTRUCTIONS = {
+    'even': 'Answer "yes" if the number is even, otherwise answer "no"',
+    'calc': 'What is the result of the expression?',
+    'gcd': 'Find the greatest common divisor of given numbers.'
+}
+ROUND_COUNT = 3
+GREETING = "Welcome to the Brain Games!\nMay I have your name? "
---
+++ brain_games/games/gcd_engine.py
@@ -0,0 +1,26 @@
+import random
+import prompt
+from math import gcd
+from brain_games.game_const import GAME_INSTRUCTIONS, ROUND_COUNT, GREETING
+
+
+def func_gcd():
+    player_name = prompt.string(f"{GREETING} ")
+    print(f"Hello, {player_name}!"
+          f"\n{GAME_INSTRUCTIONS['gcd']}")
+    for _ in range(ROUND_COUNT):
+        first_num = random.randint(1, 100)
+        second_num = random.randint(1, 55)
+
+        correct_answer = gcd(first_num, second_num)
+
+        player_answer = prompt.string(f"\nQuestion: {first_num} {second_num}"
+                                      f"\nAnswer: ")
+
+        if correct_answer == int(player_answer):
+            print("Correct!")
+        else:
+            print(f"'{player_answer}' is wrong answer ;(. Correct answer was {correct_answer}.\n"
+                  f"Let's try again, {player_name}!")
+
+    return print(f"Congratulations, {player_name}!")
